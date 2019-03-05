0698c65611af46dbf73d627a5ccbe43014421a16
Roni Estein <roni.estein@gmail.com>
2019-03-04 18:30:56 -0600

    Changed Extended testcase to Quicktools Testcase

--- src/Tests/DBTestCase.php
+++ src/Tests/DBTestCase.php
@@ -2,6 +2,7 @@
 
 namespace Quicktools\Tests;
 
+use Quicktools\Tests\TestCase;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
